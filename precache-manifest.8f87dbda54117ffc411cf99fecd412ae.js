1741c247d230da31036b56f8f903ce150768f751
Muzaffer <developerphp1995@gmail.com>
2022-10-19 04:35:05 -0700

    Updates

---
+++ precache-manifest.8f87dbda54117ffc411cf99fecd412ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b12e2849ec914744fe08",
+    "url": "/reactinfo/static/css/main.d62684d9.chunk.css"
+  },
+  {
+    "revision": "b12e2849ec914744fe08",
+    "url": "/reactinfo/static/js/main.b12e2849.chunk.js"
+  },
+  {
+    "revision": "497dc6329578ae794410",
+    "url": "/reactinfo/static/js/1.497dc632.chunk.js"
+  },
+  {
+    "revision": "f6dafb7fcdbdf07fc0e9",
+    "url": "/reactinfo/static/css/2.efd50428.chunk.css"
+  },
+  {
+    "revision": "f6dafb7fcdbdf07fc0e9",
+    "url": "/reactinfo/static/js/2.f6dafb7f.chunk.js"
+  },
+  {
+    "revision": "903e2466bb0493e70ddf",
+    "url": "/reactinfo/static/js/runtime~main.903e2466.js"
+  },
+  {
+    "revision": "0ce148dd21da784600c4ad2d86731ea9",
+    "url": "/reactinfo/static/media/logo_md.0ce148dd.jfif"
+  },
+  {
+    "revision": "e548a2c2f4a329784c5a67bf2a0dfccb",
+    "url": "/reactinfo/static/media/IMG_3290.e548a2c2.jfif"
+  },
+  {
+    "revision": "d400509d3b7b64894ee7a976dfa1e61b",
+    "url": "/reactinfo/static/media/IMG_3366.d400509d.jfif"
+  },
+  {
+    "revision": "ab0c01bddd5e19b03041a16efa36edd0",
+    "url": "/reactinfo/static/media/IMG_2544.ab0c01bd.jfif"
+  },
+  {
+    "revision": "4d2a09b1ff145c3a07b9f8aac6e86a97",
+    "url": "/reactinfo/static/media/IMG_3346.4d2a09b1.jfif"
+  },
+  {
+    "revision": "cb6b574724d5b381490284c842862925",
+    "url": "/reactinfo/index.html"
+  }
+];
